de168f5391a32a5407678fbb24bf2971145d65a8
tithxa <tithxa@syntronic.com>
2023-01-30 14:30:54 +0100

    Removed unused extension

--- Extensions/ExtensionMethods.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class ExtensionMethods
-{
-	public static void RepeatAction(this object _, int repeatCount, Action action)
-	{
-		for (int i = 0; i < repeatCount; i++)
-			action();
-	}
-}
